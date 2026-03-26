df157b3b228c20cac9594882469c3ade66b91e9a
kirtanshah001 <kirtanshah001@gmail.com>
2026-03-26 20:05:33 +0530

    UPDATE model_trainer.py

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -2,7 +2,6 @@ import os
 import sys
 from dataclasses import dataclass
 
-from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,
     GradientBoostingRegressor,
@@ -44,7 +43,6 @@ class ModelTrainer:
                 "Linear Regression": LinearRegression(),
                 "K-Neighbors Regressor": KNeighborsRegressor(),
                 "XGBRegressor": XGBRegressor(),
-                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor()
             }
             
@@ -67,11 +65,6 @@ class ModelTrainer:
                 "XGBRegressor": {
                     'learning_rate': [.1, .01, .05, .001],
                     'n_estimators': [8, 16, 32, 64, 128, 256]
-                },
-                "CatBoosting Regressor": {
-                    'depth': [6, 8, 10],
-                    'learning_rate': [0.01, 0.05, 0.1],
-                    'iterations': [30, 50, 100]
                 },
                 "AdaBoost Regressor": {
                     'learning_rate': [.1, .01, 0.05, .001],
